834c1b6b9e6707b865b84322998d3364604e5a14
unknown <zcj2518529668@163.com>
2024-03-05 22:01:53 +0800

    项目初始化

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,13 @@
 /*
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @Date: 2024-03-05 19:17:11
+ * @Date: 2024-03-05 19:19:16
  * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2024-03-05 21:58:13
- * @FilePath: \fengye7-court\src\app\page.tsx
+ * @LastEditTime: 2024-03-05 21:51:28
+ * @FilePath: \fengye7-court\src\pages\_app.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import Image from "next/image";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Fengye7Home from "@/pages/home";
+import type { AppProps } from 'next/app'
 
-export default function Home() {
-  return (
-    <Fengye7Home/>
-  )
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,21 @@
 /*
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @Date: 2024-03-05 19:17:11
+ * @Date: 2024-03-05 19:19:33
  * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2024-03-05 21:58:13
- * @FilePath: \fengye7-court\src\app\page.tsx
+ * @LastEditTime: 2024-03-05 19:19:37
+ * @FilePath: \fengye7-court\src\pages\_document.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import Image from "next/image";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Fengye7Home from "@/pages/home";
+import { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Home() {
+export default function Document() {
   return (
-    <Fengye7Home/>
+    <Html>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
   )
 }
